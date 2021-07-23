7251b96dfb63ec3064711dae64f3526ac3b7e01d
jonathan-whitehat <76190639+jonathan-whitehat@users.noreply.github.com>
2021-07-22 22:21:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload()
+{
+
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+
+	fill("#FF0000");
+	stroke("#FF0000");
+}
